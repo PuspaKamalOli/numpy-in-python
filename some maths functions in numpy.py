bb034ca57bfcf52c9b1e09e7c0d4735a2991f7a5
Puspa Kamal oli <105839410+PuspaKamalOli@users.noreply.github.com>
2022-06-29 08:37:34 +0545

    Add files via upload

---
+++ some maths functions in numpy.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+a_1 = np.array([[1, 2, 3],
+                [3, 4, 5]])
+a_2 = np.array([[3, 4, 5],
+                [6, 7, 8]])
+# sum
+print(a_1 + a_2)
+# subtraction
+print(a_2 - a_1)
+# multiplication
+print(a_1 * a_2)
+# division
+print(a_1 / a_2)
+# floor division
+print(a_1 // a_2)
+# remainder
+print(a_1 % a_2)
+# raise power
+print(a_1 ** a_2)
